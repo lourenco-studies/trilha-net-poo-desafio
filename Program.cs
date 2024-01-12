7acaef69ddbd929406f75d0473802069e221b68e
Vinicius S. Lourenco <vinicius.vsl@gmail.com>
2024-01-11 21:10:54 -0300

    Solução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456", "Tijolão", "134679", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Candy Crush");
+
+Console.WriteLine("");
+
+Smartphone iphone = new Iphone("78901", "XS", "24680", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
